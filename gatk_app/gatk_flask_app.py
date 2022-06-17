4d1f1856989b76072008dd1368bc0d1ef9a2c158
jamiesongray <67711616+jamiesongray@users.noreply.github.com>
2022-06-17 15:52:54 -0500

    Updated initial app w/ multiple endpoints

--- gatk_app/gatk_flask_app.py
+++ gatk_app/gatk_flask_app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 # stdout and stderr and returning a pid number
   
 @app.route("/run_gatk_mark_dupes", methods=["POST"])
-def handle_gatk_mark_dupes():
+def run_gatk_mark_dupes():
     rjson = request.get_json()
     ## get vals
     path_to_input_bam = rjson["input_bam_path"]
@@ -33,13 +33,15 @@ def handle_gatk_mark_dupes():
     log.flush()
     return str(proc.pid + 1)
 
-  
 @app.route("/run_gatk_splitncigar", methods=["POST"])
-def handle_gatk_splitncigar():
+def run_gatk_splitncigar():
     rjson = request.get_json()
     ## get vals
-    path_to_input_bam = rjson["input_bam_path"]
-
+    path_to_dict_fa = rjson["path_to_dict_fa"]
+    path_to_deduped_bam = rjson["path_to_deduped_bam"]
+    path_to_split_bam = rjson["path_to_split_bam"]
+    path_to_log = rjson["log_path"]
+    
     log = open(path_to_log, "w")
     
     cmd = (
@@ -52,11 +54,14 @@ def handle_gatk_splitncigar():
     return str(proc.pid + 1)
 
 @app.route("/run_gatk_labelread", methods=["POST"])
-def handle_gatk_labelread():
+def run_gatk_labelread():
     rjson = request.get_json()
     ## get vals
-    path_to_input_bam = rjson["input_bam_path"]
-
+    path_to_labeled_bam = rjson["path_to_labeled_bam"]
+    path_to_split_bam = rjson["path_to_split_bam"]
+    sample_name = rjson["sample_name"]
+    path_to_log = rjson["log_path"]
+    
     log = open(path_to_log, "w")
     
     cmd = (
@@ -69,11 +74,14 @@ def handle_gatk_labelread():
     return str(proc.pid + 1)
 
 @app.route("/run_gatk_baserecal", methods=["POST"])
-def handle_gatk_baserecal():
+def run_gatk_baserecal():
     rjson = request.get_json()
     ## get vals
-    path_to_input_bam = rjson["input_bam_path"]
-
+    path_to_dict_fa = rjson["path_to_dict_fa"]
+    path_to_labeled_bam = rjson["path_to_labeled_bam"]
+    path_to_recal_csv = rjson["path_to_recal_csv"]
+    path_to_log = rjson["log_path"]
+    
     log = open(path_to_log, "w")
     
     cmd = (
@@ -86,11 +94,15 @@ def handle_gatk_baserecal():
     return str(proc.pid + 1)
 
 @app.route("/run_gatk_bqsr", methods=["POST"])
-def handle_gatk_bqsr():
+def run_gatk_bqsr():
     rjson = request.get_json()
     ## get vals
-    path_to_input_bam = rjson["input_bam_path"]
-
+    path_to_dict_fa = rjson["path_to_dict_fa"]
+    path_to_labeled_bam = rjson["path_to_labeled_bam"]
+    path_to_recaled_bam = rjson["path_to_recaled_bam"]
+    path_to_recal_csv = rjson["path_to_recal_csv"]
+    path_to_log = rjson["log_path"]
+      
     log = open(path_to_log, "w")
     
     cmd = (
@@ -103,11 +115,14 @@ def handle_gatk_bqsr():
     return str(proc.pid + 1)
 
 @app.route("/run_gatk_halotypecaller", methods=["POST"])
-def handle_gatk_halotypecaller():
+def run_gatk_halotypecaller():
     rjson = request.get_json()
     ## get vals
-    path_to_input_bam = rjson["input_bam_path"]
-
+    path_to_vcf = rjson["path_to_vcf"]
+    path_to_dict_fa = rjson["path_to_dict_fa"]
+    path_to_recaled_bam = rjson["path_to_recaled_bam"]
+    path_to_log = rjson["log_path"]
+    
     log = open(path_to_log, "w")
     
     cmd = (
@@ -123,15 +138,18 @@ def handle_gatk_halotypecaller():
 ##	--dbsnp /mnt/iquityazurefileshare1/IQuity/GATK_Testing/gatk-workflows/inputs/Homo_sapiens_assembly19_1000genomes_decoy/Homo_sapiens_assembly19_1000genomes_decoy.dbsnp138.vcf	
 
 @app.route("/run_gatk_variantfiltration", methods=["POST"])
-def handle_gatk_variantfiltration():
+def run_gatk_variantfiltration():
     rjson = request.get_json()
     ## get vals
-    path_to_input_bam = rjson["input_bam_path"]
-
+    path_to_vcf = rjson["path_to_vcf"]
+    path_to_dict_fa = rjson["path_to_dict_fa"]
+    path_to_filtered_vcf = rjson["path_to_filtered_vcf]
+    path_to_log = rjson["log_path"]
+    
     log = open(path_to_log, "w")
     
     cmd = (
-      f"/usr/lib/gatk/gatk VariantFiltration --R {path_to_dict_fa} --V {path_to_vcf} --window 35 --cluster 3 --filter-name "FS" --filter "FS>30.0" --filter-name "QD" --filter "QD<2.0" -O {path_to_filtered_vcf}"
+      f"/usr/lib/gatk/gatk VariantFiltration --R {path_to_dict_fa} --V {path_to_vcf} --window 35 --cluster 3 --filter-name 'FS' --filter 'FS>30.0' --filter-name 'QD' --filter 'QD<2.0' -O {path_to_filtered_vcf}"
       )
     
     proc = subprocess.Popen(cmd, shell=True, stdout=log, stderr=log) 
